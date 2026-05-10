27fc6c574f4a43328c7a97a0a88c3304ce74faaa
Ben-Zion Afuta <benzi1331@gmail.com>
2026-05-10 09:29:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 // Paz Victor Maor & Ben-Zion Afuta
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         MenuManager menuManager = new MenuManager(); // Create menu manager instance
         menuManager.run(); // Start the program  
     }
